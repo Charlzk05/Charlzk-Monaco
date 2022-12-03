7e28583c065bc0f20fbff8979ec9e8919cc50002
Charlzk <104715127+Charlzk05@users.noreply.github.com>
2022-12-03 20:36:16 +0800

    Python Functions

---
+++ CharlzkAutoComplete/python.js
@@ -0,0 +1,359 @@
+function python(range) {
+    return [
+        {
+            label: "abs",
+            kind: monaco.languages.CompletionItemKind.Function,
+            insertText: "abs"
+        },
+        {
+            label: "aiter",
+            kind: monaco.languages.CompletionItemKind.Function,
+            insertText: "aiter"
+        },
+        {
+            label: "all",
+            kind: monaco.languages.CompletionItemKind.Function,
+            insertText: "all"
+        },
+        {
+            label: "any",
+            kind: monaco.languages.CompletionItemKind.Function,
+            insertText: "any"
+        },
+        {
+            label: "anext",
+            kind: monaco.languages.CompletionItemKind.Function,
+            insertText: "anext"
+        },
+        {
+            label: "ascii",
+            kind: monaco.languages.CompletionItemKind.Function,
+            insertText: "ascii"
+        },
+        {
+            label: "bin",
+            kind: monaco.languages.CompletionItemKind.Function,
+            insertText: "bin"
+        },
+        {
+            label: "bool",
+            kind: monaco.languages.CompletionItemKind.Function,
+            insertText: "bool"
+        },
+        {
+            label: "breakpoint",
+            kind: monaco.languages.CompletionItemKind.Function,
+            insertText: "breakpoint"
+        },
+        {
+            label: "bytearray",
+            kind: monaco.languages.CompletionItemKind.Function,
+            insertText: "bytearray"
+        },
+        {
+            label: "bytes",
+            kind: monaco.languages.CompletionItemKind.Function,
+            insertText: "bytes"
+        },
+        {
+            label: "callable",
+            kind: monaco.languages.CompletionItemKind.Function,
+            insertText: "callable"
+        },
+        {
+            label: "chr",
+            kind: monaco.languages.CompletionItemKind.Function,
+            insertText: "chr"
+        },
+        {
+            label: "classmethod",
+            kind: monaco.languages.CompletionItemKind.Function,
+            insertText: "classmethod"
+        },
+        {
+            label: "compile",
+            kind: monaco.languages.CompletionItemKind.Function,
+            insertText: "compile"
+        },
+        {
+            label: "complex",
+            kind: monaco.languages.CompletionItemKind.Function,
+            insertText: "complex"
+        },
+        {
+            label: "delattr",
+            kind: monaco.languages.CompletionItemKind.Function,
+            insertText: "delattr"
+        },
+        {
+            label: "dict",
+            kind: monaco.languages.CompletionItemKind.Function,
+            insertText: "dict"
+        },
+        {
+            label: "dir",
+            kind: monaco.languages.CompletionItemKind.Function,
+            insertText: "dir"
+        },
+        {
+            label: "divmod",
+            kind: monaco.languages.CompletionItemKind.Function,
+            insertText: "divmod"
+        },
+        {
+            label: "enumerate",
+            kind: monaco.languages.CompletionItemKind.Function,
+            insertText: "enumerate"
+        },
+        {
+            label: "eval",
+            kind: monaco.languages.CompletionItemKind.Function,
+            insertText: "eval"
+        },
+        {
+            label: "exec",
+            kind: monaco.languages.CompletionItemKind.Function,
+            insertText: "exec"
+        },
+        {
+            label: "filter",
+            kind: monaco.languages.CompletionItemKind.Function,
+            insertText: "filter"
+        },
+        {
+            label: "float",
+            kind: monaco.languages.CompletionItemKind.Function,
+            insertText: "float"
+        },
+        {
+            label: "format",
+            kind: monaco.languages.CompletionItemKind.Function,
+            insertText: "format"
+        },
+        {
+            label: "frozenset",
+            kind: monaco.languages.CompletionItemKind.Function,
+            insertText: "frozenset"
+        },
+        {
+            label: "getattr",
+            kind: monaco.languages.CompletionItemKind.Function,
+            insertText: "getattr"
+        },
+        {
+            label: "globals",
+            kind: monaco.languages.CompletionItemKind.Function,
+            insertText: "globals"
+        },
+        {
+            label: "hasattr",
+            kind: monaco.languages.CompletionItemKind.Function,
+            insertText: "hasattr"
+        },
+        {
+            label: "hash",
+            kind: monaco.languages.CompletionItemKind.Function,
+            insertText: "hash"
+        },
+        {
+            label: "help",
+            kind: monaco.languages.CompletionItemKind.Function,
+            insertText: "help"
+        },
+        {
+            label: "hex",
+            kind: monaco.languages.CompletionItemKind.Function,
+            insertText: "hex"
+        },
+        {
+            label: "id",
+            kind: monaco.languages.CompletionItemKind.Function,
+            insertText: "id"
+        },
+        {
+            label: "input",
+            kind: monaco.languages.CompletionItemKind.Function,
+            insertText: "input"
+        },
+        {
+            label: "int",
+            kind: monaco.languages.CompletionItemKind.Function,
+            insertText: "int"
+        },
+        {
+            label: "isinstance",
+            kind: monaco.languages.CompletionItemKind.Function,
+            insertText: "isinstance"
+        },
+        {
+            label: "issubclass",
+            kind: monaco.languages.CompletionItemKind.Function,
+            insertText: "issubclass"
+        },
+        {
+            label: "iter",
+            kind: monaco.languages.CompletionItemKind.Function,
+            insertText: "iter"
+        },
+        {
+            label: "len",
+            kind: monaco.languages.CompletionItemKind.Function,
+            insertText: "len"
+        },
+        {
+            label: "list",
+            kind: monaco.languages.CompletionItemKind.Function,
+            insertText: "list"
+        },
+        {
+            label: "locals",
+            kind: monaco.languages.CompletionItemKind.Function,
+            insertText: "locals"
+        },
+        {
+            label: "map",
+            kind: monaco.languages.CompletionItemKind.Function,
+            insertText: "map"
+        },
+        {
+            label: "max",
+            kind: monaco.languages.CompletionItemKind.Function,
+            insertText: "max"
+        },
+        {
+            label: "memoryview",
+            kind: monaco.languages.CompletionItemKind.Function,
+            insertText: "memoryview"
+        },
+        {
+            label: "min",
+            kind: monaco.languages.CompletionItemKind.Function,
+            insertText: "min"
+        },
+        {
+            label: "next",
+            kind: monaco.languages.CompletionItemKind.Function,
+            insertText: "next"
+        },
+        {
+            label: "object",
+            kind: monaco.languages.CompletionItemKind.Function,
+            insertText: "object"
+        },
+        {
+            label: "oct",
+            kind: monaco.languages.CompletionItemKind.Function,
+            insertText: "oct"
+        },
+        {
+            label: "open",
+            kind: monaco.languages.CompletionItemKind.Function,
+            insertText: "open"
+        },
+        {
+            label: "ord",
+            kind: monaco.languages.CompletionItemKind.Function,
+            insertText: "ord"
+        },
+        {
+            label: "pow",
+            kind: monaco.languages.CompletionItemKind.Function,
+            insertText: "pow"
+        },
+        {
+            label: "print",
+            kind: monaco.languages.CompletionItemKind.Function,
+            insertText: "print"
+        },
+        {
+            label: "property",
+            kind: monaco.languages.CompletionItemKind.Function,
+            insertText: "property"
+        },
+        {
+            label: "range",
+            kind: monaco.languages.CompletionItemKind.Function,
+            insertText: "range"
+        },
+        {
+            label: "repr",
+            kind: monaco.languages.CompletionItemKind.Function,
+            insertText: "repr"
+        },
+        {
+            label: "reversed",
+            kind: monaco.languages.CompletionItemKind.Function,
+            insertText: "reversed"
+        },
+        {
+            label: "round",
+            kind: monaco.languages.CompletionItemKind.Function,
+            insertText: "round"
+        },
+        {
+            label: "set",
+            kind: monaco.languages.CompletionItemKind.Function,
+            insertText: "set"
+        },
+        {
+            label: "setattr",
+            kind: monaco.languages.CompletionItemKind.Function,
+            insertText: "setattr"
+        },
+        {
+            label: "slice",
+            kind: monaco.languages.CompletionItemKind.Function,
+            insertText: "slice"
+        },
+        {
+            label: "sorted",
+            kind: monaco.languages.CompletionItemKind.Function,
+            insertText: "sorted"
+        },
+        {
+            label: "staticmethod",
+            kind: monaco.languages.CompletionItemKind.Function,
+            insertText: "staticmethod"
+        },
+        {
+            label: "str",
+            kind: monaco.languages.CompletionItemKind.Function,
+            insertText: "str"
+        },
+        {
+            label: "sum",
+            kind: monaco.languages.CompletionItemKind.Function,
+            insertText: "sum"
+        },
+        {
+            label: "super",
+            kind: monaco.languages.CompletionItemKind.Function,
+            insertText: "super"
+        },
+        {
+            label: "tuple",
+            kind: monaco.languages.CompletionItemKind.Function,
+            insertText: "tuple"
+        },
+        {
+            label: "type",
+            kind: monaco.languages.CompletionItemKind.Function,
+            insertText: "type"
+        },
+        {
+            label: "vars",
+            kind: monaco.languages.CompletionItemKind.Function,
+            insertText: "vars"
+        },
+        {
+            label: "zip",
+            kind: monaco.languages.CompletionItemKind.Function,
+            insertText: "zip"
+        },
+        {
+            label: "__import__",
+            kind: monaco.languages.CompletionItemKind.Function,
+            insertText: "__import__"
+        }
+    ];
+}
